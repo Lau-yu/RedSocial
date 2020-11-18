fa9ffe8e50b2079cfaf83f42db01314c5df9ce88
Lau-yu <yujraquisbertl@gmail.com>
2020-11-18 00:12:01 -0400

    1y2sprint

---
+++ app/src/main/java/com/optic/socialmediagamer/fragments/HomeFragment.java
@@ -0,0 +1,122 @@
+package com.optic.socialmediagamer.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
+import com.optic.socialmediagamer.R;
+import com.optic.socialmediagamer.activities.MainActivity;
+import com.optic.socialmediagamer.activities.PostActivity;
+import com.optic.socialmediagamer.adapters.PostsAdapter;
+import com.optic.socialmediagamer.models.Post;
+import com.optic.socialmediagamer.providers.AuthProvider;
+import com.optic.socialmediagamer.providers.PostProvider;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    View mView;
+    FloatingActionButton mFab;
+    Toolbar mToolbar;
+    AuthProvider mAuthProvider;
+    RecyclerView mRecyclerView;
+    PostProvider mPostProvider;
+    PostsAdapter mPostsAdapter;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
+        mFab = mView.findViewById(R.id.fab);
+        mToolbar = mView.findViewById(R.id.toolbar);
+        mRecyclerView = mView.findViewById(R.id.recyclerViewHome);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        ((AppCompatActivity) getActivity()).setSupportActionBar(mToolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Publicaciones");
+
+        setHasOptionsMenu(true);
+        mAuthProvider = new AuthProvider();
+        mPostProvider = new PostProvider();
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToPost();
+            }
+        });
+        return mView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Query query = mPostProvider.getAll();
+        FirestoreRecyclerOptions<Post> options =
+                new FirestoreRecyclerOptions.Builder<Post>()
+                        .setQuery(query, Post.class)
+                        .build();
+        mPostsAdapter = new PostsAdapter(options, getContext());
+        mRecyclerView.setAdapter(mPostsAdapter);
+        mPostsAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mPostsAdapter.stopListening();
+    }
+
+    private void goToPost() {
+        Intent intent = new Intent(getContext(), PostActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemLogout) {
+            logout();
+        }
+
+        return true;
+    }
+
+    private void logout() {
+        mAuthProvider.logout();
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
